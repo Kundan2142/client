122de5534e4e1a2ba9d77a9b2caf075e44d2a465
Kundan2142 <E19CSE078@bennett.edu.in>
2023-07-15 05:31:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const message="kundan"
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App
